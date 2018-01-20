32b510893fd92a13f5226c7955cb22d94a4620d3
greg_huang <greg_huang@trend.com.tw>
2018-01-20 21:00:18 +0800

    train model for parent control

---
+++ soruce/pc_feature_extract.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import hashlib
+from sklearn.preprocessing import StandardScaler
+
+def hashEventCase(df):
+	 # Hash the eventcase as md5
+	 idx = [hashlib.md5(val).hexdigest() for val in df['__EVENTCASE__']]
+	 return idx
+
+def ratio(df, category):
+	df.loc[:,[category]] = df[category]/df['__PARENTALCONTROL__total_event']
+	return df[category].values
+
+def scale(data):
+	data_scaled = StandardScaler().fit_transform(data.values.reshape(-1,1).astype('float64'))
+	return data_scaled.reshape(-1)
+	
+
+def transform(df):
+	# ntfid_arp = get_ratio(df, '__NOTIFICATION__payload_notification_id_PUSH_NOTIFICATION_ARP_SPOOFING_FOUND')
+
+	data = {
+	'_label' : df['__PARENTALCONTROL__station_final_status'].values,
+	'_total': scale(df['__PARENTALCONTROL__total_event']),
+	'_rate': df['__PARENTALCONTROL__rate'].values,
+	'_entropy_hour': df['__PARENTALCONTROL__entropy_for_hours_of_day'].values,
+	'cnt_app': df['__PARENTALCONTROL__distinct_payload_app_id'].values,
+	'cnt_device': df['__PARENTALCONTROL__distinct_payload_device_id'].values,
+	'scaled_kid_back_home': scale(df['__PARENTALCONTROL__event_type_kids_back_to_home']),
+	'scaled_inapp_detected': scale(df['__PARENTALCONTROL__event_type_inappropriate_app_detected']),
+	'scaled_net_limit' : scale(df['__PARENTALCONTROL__event_type_network_time_limited']),
+	'scaled_web_filtered': scale(df['__PARENTALCONTROL__event_type_web_site_filtered']),
+	'scaled_cat_0': scale(df['__PARENTALCONTROL__payload_cat_id_0']),
+	'scaled_cat_1': scale(df['__PARENTALCONTROL__payload_cat_id_1']),
+	'ratio_web_filtered': ratio(df, '__PARENTALCONTROL__event_type_web_site_filtered'),
+	'ratio_net_limit': ratio(df, '__PARENTALCONTROL__event_type_network_time_limited'),
+	'ratio_inapp_detected': ratio(df, '__PARENTALCONTROL__event_type_inappropriate_app_detected'),
+	'ratio_kid_back_home': ratio(df, '__PARENTALCONTROL__event_type_kids_back_to_home'),
+	# 'scaled_action_block': scale(df['__PARENTALCONTROL__payload_action_block']),
+	# 'scaled_action_accept': scale(df['__PARENTALCONTROL__payload_action_accept']),
+	}
+	return data
+
+
+def main():
+	print('\n')
+	print("------------Load Data------------\n")
+	trainDF = pd.read_csv("../data/training/Train_extracted_parental_control_feature_2018-01-17_21-20-30.csv")
+	testDF = pd.read_csv("../data/testing/Test_extracted_parental_control_feature_2018-01-17_21-20-30.csv")
+
+	trainID = hashEventCase(trainDF)
+	testID = hashEventCase(testDF)
+	
+	trainData = pd.DataFrame(transform(trainDF), index = trainID)
+	testData = pd.DataFrame(transform(testDF), index = testID)
+	print trainData.describe()
+
+	trainData.to_csv('../data/feature/pc_training_v1.csv')
+	testData.to_csv('../data/feature/pc_testing_v1.csv')
+
+
+if __name__ == '__main__':
+	main()
---
+++ soruce/pc_rf_training_all.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.externals import joblib
+import seaborn as sns
+
+def prepare(df):
+	x = df.drop('_label', axis=1)
+	y = df.loc[:,'_label']
+	return x, y
+
+def training(x, y):
+	print("\n-----------RF Classifier-----------")
+	model = RandomForestClassifier(random_state=0)
+	model.fit(x, y)
+	return model
+
+def validate(y_train_pred, y_train, y_test_pred, y_test):
+	print "Accuracy :: ", accuracy_score(y_test, y_test_pred)
+	print " Confusion matrix ::\n", confusion_matrix(y_test, y_test_pred)
+
+	fpr_rf_train, tpr_rf_train, _ = roc_curve(y_train, y_train_pred)
+	fpr_rf, tpr_rf, _ = roc_curve(y_test, y_test_pred)
+	plt.figure(1)
+	plt.plot([0, 1], [0, 1], 'k--')
+	plt.plot(fpr_rf_train, tpr_rf_train, label='PC_Train')
+	plt.plot(fpr_rf, tpr_rf, label='PC_Test')
+	plt.xlabel('False positive rate')
+	plt.ylabel('True positive rate')
+	plt.title('ROC curve')
+	plt.legend(loc='best')
+	plt.show()
+
+def main():
+	trainDF = pd.read_csv('../data/feature/pc_training_v1.csv', index_col=0)
+	testDF = pd.read_csv('../data/feature/pc_testing_v1.csv', index_col=0)
+	allDF = pd.concat([trainDF, testDF])
+	X, Y = prepare(allDF)
+	# Split data into train set and test set
+	x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.15, random_state=123, stratify=Y)
+	print x_test.size
+	
+	tmodel = training(x_train, y_train)
+	y_train_pred = tmodel.predict(x_train)
+	y_test_pred = tmodel.predict(x_test)
+	validate(y_train_pred, y_train ,y_test_pred, y_test)
+
+
+if __name__ == "__main__":
+	main()
